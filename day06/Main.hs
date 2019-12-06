6b43370e65ea144bfc30ea3ef20b89d08f6e480f
Arjan de Pooter <mail@arjandepooter.nl>
2019-12-06 21:35:17 +0100

    Solve day 6 part 2

--- day06/Main.hs
+++ day06/Main.hs
@@ -1,6 +1,9 @@
-import           Data.Map.Lazy (empty, findWithDefault, fromListWith)
-import           Data.Tree     (Tree, levels, unfoldTree)
-import           Debug.Trace   (trace)
+{-# LANGUAGE NamedFieldPuns #-}
+
+import           Control.Applicative ((<|>))
+import           Data.Map.Lazy       (empty, findWithDefault, fromListWith)
+import           Data.Maybe          (fromMaybe)
+import           Data.Tree           (Tree (..), levels, unfoldTree)
 
 buildTree :: [(String, String)] -> Tree String
 buildTree l = unfoldTree f "COM"
@@ -8,9 +11,30 @@ buildTree l = unfoldTree f "COM"
     mapping = fromListWith (++) (fmap (fmap (: [])) l)
     f node = (node, findWithDefault [] node mapping)
 
+findPath :: Eq a => a -> Tree a -> Maybe [a]
+findPath needle Node {rootLabel, subForest} =
+  if needle == rootLabel
+    then Just []
+    else (rootLabel :) <$> foldr (<|>) Nothing (findPath needle <$> subForest)
+
+stripEqual :: Eq a => [a] -> [a] -> ([a], [a])
+stripEqual a [] = (a, [])
+stripEqual [] b = ([], b)
+stripEqual (a:as) (b:bs) =
+  if a == b
+    then stripEqual as bs
+    else (a : as, b : bs)
+
 solve1 :: [(String, String)] -> Int
 solve1 =
   sum . fmap (uncurry (*)) . zip [0 ..] . fmap length . levels . buildTree
+
+solve2 :: [(String, String)] -> Int
+solve2 ls = length . uncurry (++) $ stripEqual path1 path2
+  where
+    tree = buildTree ls
+    path1 = fromMaybe [] $ findPath "YOU" tree
+    path2 = fromMaybe [] $ findPath "SAN" tree
 
 parseLine :: String -> (String, String)
 parseLine = (\(s1:s2:_) -> (s1, s2)) . splitOn ')'
@@ -31,3 +55,4 @@ main :: IO ()
 main = do
   lns <- fmap parseLine . lines <$> getContents
   print $ solve1 lns
+  print $ solve2 lns
