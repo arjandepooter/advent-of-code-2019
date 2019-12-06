3a486d994bde2c5bd42b408e35e7b189e3bcc595
Arjan de Pooter <mail@arjandepooter.nl>
2019-12-06 15:35:51 +0100

    Solved day 5 part 1

---
+++ day05/Main.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE NamedFieldPuns  #-}
+{-# LANGUAGE RecordWildCards #-}
+
+import           Control.Monad.State
+import           Data.Char           (digitToInt)
+import           Data.Sequence       (Seq (..), fromList, index, update)
+import           Debug.Trace         (trace)
+
+type Pointer = Int
+
+type Opcode = Int
+
+type Program = Seq Int
+
+data ArgMode
+  = Position
+  | Immediate
+
+data Runtime =
+  Runtime
+    { program :: Program
+    , pointer :: Pointer
+    , inputs  :: [Int]
+    , outputs :: [Int]
+    }
+  deriving (Show)
+
+(-->) :: Program -> Pointer -> Int
+(-->) = index
+
+infixl 2 -->
+
+(*->) :: Program -> Pointer -> Int
+prg *-> ptr = prg --> (prg --> ptr)
+
+infixl 2 *->
+
+parseCommand :: Int -> (Opcode, [ArgMode])
+parseCommand =
+  (\(arg3:arg2:arg1:opcode) ->
+     (read opcode, fmap charToArgMode [arg1, arg2, arg3])) .
+  (\l -> replicate (5 - length l) '0' ++ l) . show
+  where
+    charToArgMode :: Char -> ArgMode
+    charToArgMode '1' = Immediate
+    charToArgMode _   = Position
+
+argOp :: ArgMode -> (Program -> Pointer -> Int)
+argOp Immediate = (-->)
+argOp Position  = (*->)
+
+mathOperation :: (Int -> Int -> Int) -> [ArgMode] -> State Runtime Bool
+mathOperation op argModes = do
+  Runtime {..} <- get
+  let arg1 = argOp (head argModes) program (pointer + 1)
+  let arg2 = argOp (argModes !! 1) program (pointer + 2)
+  let target = program --> (pointer + 3)
+  let newProgram = update target (arg1 `op` arg2) program
+  put (Runtime newProgram (pointer + 4) inputs outputs)
+  return False
+
+exit :: [ArgMode] -> State Runtime Bool
+exit = const (return True)
+
+input :: [ArgMode] -> State Runtime Bool
+input _ = do
+  Runtime {..} <- get
+  let (ip:ips) = inputs
+  let target = program --> (pointer + 1)
+  let newProgram = update target ip program
+  put (Runtime newProgram (pointer + 2) ips outputs)
+  return False
+
+output :: [ArgMode] -> State Runtime Bool
+output (argMode:_) = do
+  Runtime {..} <- get
+  let value = argOp argMode program (pointer + 1)
+  put (Runtime program (pointer + 2) inputs (value : outputs))
+  return False
+
+opcodeCommand :: Opcode -> ([ArgMode] -> State Runtime Bool)
+opcodeCommand opcode =
+  case opcode of
+    1  -> mathOperation (+)
+    2  -> mathOperation (*)
+    3  -> input
+    4  -> output
+    99 -> exit
+    _  -> error $ "Unknown opcode found: " ++ show opcode
+
+runCommand :: State Runtime Bool
+runCommand = do
+  Runtime {program, pointer} <- get
+  let cmd = program --> pointer
+  let (opcode, argModes) = parseCommand cmd
+  let command = opcodeCommand opcode
+  command argModes
+
+runUntilFinished :: State Runtime [Int]
+runUntilFinished = do
+  finished <- runCommand
+  if finished
+    then do
+      Runtime {outputs} <- get
+      return outputs
+    else runUntilFinished
+
+runProgram :: [Int] -> Program -> [Int]
+runProgram inputs prg = evalState runUntilFinished (Runtime prg 0 inputs [])
+
+solve1 :: Program -> Int
+solve1 = head . runProgram [1]
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn sep = foldr folder []
+  where
+    folder cur [] =
+      if cur == sep
+        then [[]]
+        else [[cur]]
+    folder cur (curPart:rest) =
+      if cur == sep
+        then [] : curPart : rest
+        else (cur : curPart) : rest
+
+parseInput :: String -> Program
+parseInput = fromList . fmap read . splitOn ','
+
+main :: IO ()
+main = do
+  prog <- parseInput <$> getContents
+  print $ solve1 prog
