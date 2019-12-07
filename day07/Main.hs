73d7ac3411bb597ad9d45f643ecfe7c2d54e0378
Arjan de Pooter <mail@arjandepooter.nl>
2019-12-07 13:05:17 +0100

    HLint

--- day07/Main.hs
+++ day07/Main.hs
@@ -1,12 +1,9 @@
-{-# LANGUAGE NamedFieldPuns  #-}
-{-# LANGUAGE RecordWildCards #-}
-
 import           AOC.IntComputer (Program, parseInput, runProgram)
 import           AOC.Utils       (splitOn)
 import           Data.List       (permutations)
 
 runAmplifier :: Program -> [Int] -> Int
-runAmplifier prg = foldl ((\p s -> head $ runProgram [s, p] prg)) 0
+runAmplifier prg = foldl (\p s -> head $ runProgram [s, p] prg) 0
 
 solve1 :: Program -> Int
 solve1 prg = maximum $ fmap (runAmplifier prg) (permutations [0 .. 4])
@@ -18,3 +15,4 @@ main :: IO ()
 main = do
   prog <- parseInput <$> getContents
   print $ solve1 prog
+  print $ solve2 prog
