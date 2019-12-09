e81c620fadb4d2b1b64e08f35a843d3b180f88b2
Arjan de Pooter <mail@arjandepooter.nl>
2019-12-09 18:10:36 +0100

    Fixed day 7 for new IntComputer implementation

--- day07/Main.hs
+++ day07/Main.hs
@@ -4,13 +4,13 @@ import           AOC.IntComputer
 import           AOC.Utils           (splitOn)
 import           Control.Monad.State (runState)
 import           Data.List           (permutations)
-import           Data.Sequence       (fromList)
 
 runAmplifier :: Program -> [Int] -> Int
 runAmplifier prg = foldl (\p s -> head $ runProgram [s, p] prg) 0
 
 setInput :: Runtime -> [Int] -> Runtime
-setInput Runtime {..} ips = Runtime program pointer (inputs ++ ips) outputs
+setInput Runtime {..} ips =
+  Runtime memory pointer relativeBase (inputs ++ ips) outputs
 
 runAmplifierLoop :: Program -> [Int] -> Int
 runAmplifierLoop prg codes =
@@ -19,12 +19,12 @@ runAmplifierLoop prg codes =
   takeWhile (\(_, finished, _) -> not finished) . scanl f (0, False, runtimes) $
   [0 ..]
   where
-    runtimes = fmap (\c -> Runtime prg 0 [c] []) codes
+    runtimes = fmap (initialize prg . return) codes
     f :: (Int, Bool, [Runtime]) -> a -> (Int, Bool, [Runtime])
     f (input, _, rt:rts) _ =
       let (finished, Runtime {..}) =
             runState runUntilOutput (setInput rt [input])
-       in (head outputs, finished, rts ++ [Runtime program pointer [] []])
+       in (head outputs, finished, rts ++ [Runtime memory pointer 0 [] []])
 
 solve1 :: Program -> Int
 solve1 prg = maximum $ fmap (runAmplifier prg) (permutations [0 .. 4])
