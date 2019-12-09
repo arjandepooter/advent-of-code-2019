b25de3b550aa0a7230ad913576fac12a6dcee4bc
Arjan de Pooter <mail@arjandepooter.nl>
2019-12-09 18:09:53 +0100

    Implemented relative arg mode & switched to IntMap for keeping memory

--- AOC/IntComputer.hs
+++ AOC/IntComputer.hs
@@ -14,34 +14,38 @@ module AOC.IntComputer
 import           AOC.Utils           (splitOn)
 import           Control.Monad.State
 import           Data.Char           (digitToInt)
-import           Data.Sequence       (Seq (..), fromList, index, update)
+import           Data.IntMap         (IntMap, alter, findWithDefault, fromList)
 import           Prelude             hiding (compare)
 
 type Pointer = Int
 
 type Opcode = Int
 
-type Program = Seq Int
+type Program = [Int]
+
+type Memory = IntMap Int
 
 data ArgMode
   = Position
   | Immediate
+  | Relative
 
 data Runtime =
   Runtime
-    { program :: Program
-    , pointer :: Pointer
-    , inputs  :: [Int]
-    , outputs :: [Int]
+    { memory       :: Memory
+    , pointer      :: Pointer
+    , relativeBase :: Int
+    , inputs       :: [Int]
+    , outputs      :: [Int]
     }
   deriving (Show)
 
-(-->) :: Program -> Pointer -> Int
-(-->) = index
+(-->) :: Memory -> Pointer -> Int
+(-->) = flip $ findWithDefault 0
 
 infixl 2 -->
 
-(*->) :: Program -> Pointer -> Int
+(*->) :: Memory -> Pointer -> Int
 prg *-> ptr = prg --> (prg --> ptr)
 
 infixl 2 *->
@@ -54,52 +58,65 @@ parseCommand =
   where
     charToArgMode :: Char -> ArgMode
     charToArgMode '1' = Immediate
+    charToArgMode '2' = Relative
     charToArgMode _   = Position
-
-argOp :: ArgMode -> (Program -> Pointer -> Int)
-argOp Immediate = (-->)
-argOp Position  = (*->)
 
 getArg :: [ArgMode] -> Int -> State Runtime Int
 getArg argModes offset = do
-  Runtime {program, pointer} <- get
+  Runtime {memory, pointer} <- get
   let argPointer = pointer + 1 + offset
   let argMode = argModes !! offset
-  let op = argOp argMode
-  return (program `op` argPointer)
+  case argMode of
+    Immediate -> return (memory --> argPointer)
+    Position -> return (memory *-> argPointer)
+    Relative -> do
+      Runtime {relativeBase} <- get
+      return (memory --> (relativeBase + (memory --> argPointer)))
+
+writeTarget :: [ArgMode] -> Int -> Int -> State Runtime ()
+writeTarget argModes offset value = do
+  Runtime {memory, pointer, relativeBase} <- get
+  let targetPointer = memory --> pointer + 1 + offset
+  let argMode = argModes !! offset
+  let target =
+        case argMode of
+          Relative -> relativeBase + targetPointer
+          _        -> targetPointer
+  let newMemory = alter (const $ Just value) target memory
+  updateMemory newMemory
+  return ()
 
 movePointer :: Int -> State Runtime ()
 movePointer newPointer = do
   Runtime {..} <- get
-  put (Runtime program newPointer inputs outputs)
+  put (Runtime memory newPointer relativeBase inputs outputs)
   return ()
 
-updateProgram :: Program -> State Runtime ()
-updateProgram newProgram = do
+updateMemory :: Memory -> State Runtime ()
+updateMemory newMemory = do
   Runtime {..} <- get
-  put (Runtime newProgram pointer inputs outputs)
+  put (Runtime newMemory pointer relativeBase inputs outputs)
 
 popInput :: State Runtime Int
 popInput = do
   Runtime {..} <- get
   let (ip:ips) = inputs
-  put (Runtime program pointer ips outputs)
+  put (Runtime memory pointer relativeBase ips outputs)
   return ip
 
 addOutput :: Int -> State Runtime ()
 addOutput o = do
   Runtime {..} <- get
-  put (Runtime program pointer inputs (o : outputs))
+  put (Runtime memory pointer relativeBase inputs (o : outputs))
   return ()
 
 mathOperation :: (Int -> Int -> Int) -> [ArgMode] -> State Runtime Bool
 mathOperation op argModes = do
-  Runtime {program, pointer} <- get
+  Runtime {memory, pointer} <- get
   arg1 <- getArg argModes 0
   arg2 <- getArg argModes 1
-  let target = program --> (pointer + 3)
-  let newProgram = update target (arg1 `op` arg2) program
-  updateProgram newProgram
+  let target = memory --> (pointer + 3)
+  writeTarget argModes 2 (arg1 `op` arg2)
   movePointer (pointer + 4)
   return False
 
@@ -108,12 +125,11 @@ exit = const (return True)
 
 input :: [ArgMode] -> State Runtime Bool
 input argModes = do
-  Runtime {pointer, program} <- get
+  Runtime {pointer, memory} <- get
   ip <- popInput
-  let target = program --> (pointer + 1)
-  let newProgram = update target ip program
+  let target = memory --> (pointer + 1)
+  writeTarget argModes 0 ip
   movePointer (pointer + 2)
-  updateProgram newProgram
   return False
 
 output :: [ArgMode] -> State Runtime Bool
@@ -138,19 +154,25 @@ jumpIf cmp argModes = do
 
 compare :: (Int -> Int -> Bool) -> [ArgMode] -> State Runtime Bool
 compare cmp argModes = do
-  Runtime {pointer, program} <- get
+  Runtime {pointer, memory} <- get
   arg1 <- getArg argModes 0
   arg2 <- getArg argModes 1
-  let target = program --> pointer + 3
-  let newProgram =
-        update
-          target
-          (if arg1 `cmp` arg2
-             then 1
-             else 0)
-          program
-  updateProgram newProgram
+  let target = memory --> pointer + 3
+  writeTarget
+    argModes
+    2
+    (if arg1 `cmp` arg2
+       then 1
+       else 0)
   movePointer (pointer + 4)
+  return False
+
+setRelativeBase :: [ArgMode] -> State Runtime Bool
+setRelativeBase argModes = do
+  arg1 <- getArg argModes 0
+  Runtime {..} <- get
+  put (Runtime memory pointer (relativeBase + arg1) inputs outputs)
+  movePointer (pointer + 2)
   return False
 
 opcodeCommand :: Opcode -> ([ArgMode] -> State Runtime Bool)
@@ -164,13 +186,14 @@ opcodeCommand opcode =
     6  -> jumpIf False
     7  -> compare (<)
     8  -> compare (==)
+    9  -> setRelativeBase
     99 -> exit
     _  -> error $ "Unknown opcode found: " ++ show opcode
 
 runCommand :: State Runtime Bool
 runCommand = do
-  Runtime {program, pointer} <- get
-  let cmd = program --> pointer
+  Runtime {memory, pointer} <- get
+  let cmd = memory --> pointer
   let (opcode, argModes) = parseCommand cmd
   let command = opcodeCommand opcode
   command argModes
@@ -195,10 +218,10 @@ runUntilOutput = do
            else return False
 
 initialize :: Program -> [Int] -> Runtime
-initialize prg = flip (Runtime prg 0) []
+initialize prg = flip (Runtime (fromList $ zip [0 ..] prg) 0 0) []
 
 runProgram :: [Int] -> Program -> [Int]
 runProgram inputs prg = evalState runUntilFinished (initialize prg inputs)
 
 parseInput :: String -> Program
-parseInput = fromList . fmap read . splitOn ','
+parseInput = fmap read . splitOn ','
